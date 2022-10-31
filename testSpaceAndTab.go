171fef7bc41b6b38c309ec2cc390f056877c3334
HatsuneMona <songmiao39@foxmail.com>
2022-10-31 11:48:52 +0800

    test : test space by using Goland git commit.

---
+++ testSpaceAndTab.go
@@ -0,0 +1,13 @@
+package main
+
+// This file is create by Goland
+// git commit by using Goland
+
+// I'm sure that I use four space but not a tab
+func testSpace() {
+	// This is four space.
+
+	for false {
+		// This is double four space.
+	}
+}
